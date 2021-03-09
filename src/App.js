e557cd247098862e83e49aec76bf98f8cf656760
Sumen Tse <sumen.tse@gmail.com>
2021-03-09 14:17:58 -0500

    chore: added routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext, Suspense, lazy } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { AuthContext } from "./context/Auth";
+import SignUp from "./pages/Signup";
+import LoadingScreen from "./components/LoadingScreen";
+const Game = lazy(() => import("./pages/Game"));
 
-function App() {
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { authenticated } = useContext(AuthContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Route
+      {...rest}
+      render={(props) => {
+        return authenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+        );
+      }}
+    />
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <Suspense fallback={<LoadingScreen />}>
+        <Switch>
+          <Route path="/signup" component={SignUp} />
+          <PrivateRoute path="/game" component={Game} />
+          <Route path="*">
+            <div>Could not find any matches</div>
+          </Route>
+        </Switch>
+      </Suspense>
     </div>
   );
-}
+};
 
 export default App;
